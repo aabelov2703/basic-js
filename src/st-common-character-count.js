23e849ff5d668e276e692d82219303c31e7a8546
aabelov2703 <alex.belov.2703@gmail.com>
2021-10-01 00:00:00 +0300

    add:  partition decision

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,11 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  return s1.split('').filter(item => {
+    if (s2.indexOf(item) >= 0) {
+      s2 = s2.replace(item, '');
+      return true
+    } else return false;
+  }).length
 }
