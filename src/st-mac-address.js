4b0cb6a39917a820b55fa23f751cf2af424a6928
aabelov2703 <alex.belov.2703@gmail.com>
2021-10-01 04:57:16 +0300

    add:  partition decision

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,6 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  return n.split('-').filter(item => item.length === 2 && (parseInt(item, 16) || item === '00')).length === 6
 }
