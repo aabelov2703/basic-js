96c584b4b699bd8c525e8f885b4a1e561195ad98
aabelov2703 <alex.belov.2703@gmail.com>
2021-10-01 04:21:59 +0300

    add:  partition decision

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let prev = str[0];
+  let res = '';
+  let cnt = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (prev === str[i]) {
+      cnt++
+    }
+    else {
+      res = `${res}${cnt === 1 ? '' : cnt}${prev}`;
+      cnt = 1;
+      prev = str[i]
+    }
+  }
+  return str === '' ? '' : `${res}${cnt === 1 ? '' : cnt}${prev}`;
 }
