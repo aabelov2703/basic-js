966993b3dba2317941f3465e55ebba0c715613fd
aabelov2703 <alex.belov.2703@gmail.com>
2021-10-01 04:39:05 +0300

    add:  partition decision

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  let res = []
+  names.forEach((item, index) => {
+    if (res.includes(item)) {
+      let i = names.slice(0, index).filter((file) => file === item).length;
+      res.push(`${item}(${i === 0 ? 1 : i})`)
+    } else {
+      res.push(item)
+    }
+  });
+  return res;
 }
