c0358eea4646f57b57491a3ce8498b28781c88c9
aabelov2703 <alex.belov.2703@gmail.com>
2021-10-02 05:48:46 +0300

    add: partition decision

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (mode = true) {
+    this.mode = mode;
+    this.latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  crypto(message, key, toCode = true) {
+    if (!message || !key) throw new Error("Incorrect arguments!");
+  
+    let res = [];
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    let kInd = 0;
+    message.split('').forEach((letter, index) => {
+      if (this.latin.indexOf(letter) >= 0) {
+        if (toCode) res.push(this.latin[(this.latin.indexOf(letter) + this.latin.indexOf(key[kInd])) % 26]);
+        if (!toCode) res.push(this.latin[(this.latin.indexOf(letter) - this.latin.indexOf(key[kInd]) + 26) % 26]);
+        kInd = kInd + 1 === key.length ? 0 : kInd + 1
+      } else {
+        res.push(letter);
+      }
+    })
+    return this.mode ? res.join('') : res.reverse().join('')
+  }
+
+  encrypt(message, key) {
+    return this.crypto(message, key);
+  }
+  
+  decrypt(encryptedMessage, key) {
+    return this.crypto(encryptedMessage, key, false);
   }
 }
